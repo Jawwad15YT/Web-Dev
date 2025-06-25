3c8e4bd264d90b2cfd01dd571ea57f0b2b17015a
Jawwad15YT <53828972+Jawwad15YT@users.noreply.github.com>
2025-06-25 22:46:08 +0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,40 @@
+// class Car{
+//     color = "Blue";
+//     window = 5;
+//     seat = 10;
+//     engine = "A1";
+// }
+
+// bmw = new Car()
+// console.log(bmw.window)
+// console.log(bmw.seat)
+// ferrari = new Car()
+// console.log(ferrari.engine)
+
+
+// class laptop{
+     
+    
+
+//     constructor(Color,Price){
+//         this.Color = Color;
+//         this.Price = Price
+//     }
+// }
+
+// hp = new laptop("Silver",50600)
+// lenovo = new laptop("Black",10000)
+
+// console.log(hp.Color)
+// console.log(lenovo.Color)
+
+class A{
+    x = 5
+}
+
+class B extends A{
+
+}
+
+obj = new B()
+console.log(obj.x)
